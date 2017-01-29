feb7bad4281326d86cd24f605240c55393c4a1c2
Tom Flídr <tomflidr@gmail.com>
2017-01-29 11:59:08 +0100

    hotfix for cases when REQUEST_URI is missing in REDIRECT_URI and there is only '/index.php' (or requesting script substring) contained at the end

--- src/MvcCore/Ext/Request/ApacheDpi.php
+++ src/MvcCore/Ext/Request/ApacheDpi.php
@@ -31,11 +31,28 @@ class ApacheDpi extends \MvcCore\Request
 	protected function initBasePath () {
 		parent::initBasePath();
 		if (isset($this->serverGlobals['REDIRECT_URL'])) {
+			// fill 'REDIRECT_URL' and 'REQUEST_URI':
+			// '/domains/www.mydomain.com/requested/app/path' and '/requested/app/path'
 			$redUrl = $this->serverGlobals['REDIRECT_URL'];
 			$reqUrl = $this->serverGlobals['REQUEST_URI'];
+			// remove possible query string from REQUEST_URI
 			$reqUrlQPos = mb_strpos($reqUrl, '?');
 			if ($reqUrlQPos !== FALSE) $reqUrl = mb_substr($reqUrl, 0, $reqUrlQPos);
+			// check if 'REDIRECT_URL' contains 'REQUEST_URI' at the very end
+			$redUrlContainsReqUrlAtTheEnd = mb_strpos($redUrl, $reqUrl) === mb_strlen($redUrl) - mb_strlen($reqUrl);
+			if (!$redUrlContainsReqUrlAtTheEnd) {
+				// if not and in 'REDIRECT_URL' is at the end '/index.php' - fix it back
+				$redUrlWithoutIndexPhpLength = mb_strlen($redUrl) - mb_strlen($this->ScriptName);
+				$redUrlScriptNameAtTheEnd = mb_strpos($redUrl, $this->ScriptName) === $redUrlWithoutIndexPhpLength;
+				if ($redUrlScriptNameAtTheEnd) {
+					$redUrl = mb_substr($redUrl, 0, $redUrlWithoutIndexPhpLength) . $reqUrl;
+				}
+			}
+			// remove all 'REQUEST_URI' characters from the very end of 'REDIRECT_URL'
 			$diffUrl = mb_substr($redUrl, 0, mb_strlen($redUrl) - mb_strlen($reqUrl));
+			// if the rested string length is bigger than zero - fix base path 
+			// remove from BasePath a sub path - the sub path between original apache document root
+			// and current application root, witch is cause by Apache .htaccess [DPI] flag (discard path)
 			$diffUrlLen = mb_strlen($diffUrl);
 			if ($diffUrlLen > 0) {
 				$this->BasePath = mb_substr($this->BasePath, $diffUrlLen);
